0e9b6febda0eb729b37dc24e18867055f4579cf4
Vansh912 <89155230+Vansh912@users.noreply.github.com>
2021-12-11 11:51:04 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	float a,b,product;
+	
+	cout<<"enter the first number ";
+	cin>>a;
+	
+	cout<<"enter the second number ";
+	cin>>b;
+	
+	product = a*b;
+	
+	cout<<"product is "<<product;
+	
+	return 0;
+	
+	
+}
